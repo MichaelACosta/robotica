369d2929519e38f411db5114d1b9eff00f3cb98a
Michael Costa <macosta@inf.ufpel.edu.br>
2018-09-04 16:34:11 -0300

    refatoração no código de movimento
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- moving/moving.ino
+++ moving/moving.ino
@@ -17,24 +17,19 @@ void loop() {
     switch (control) {
       case 'd':
         analogWrite(pwmX, 80);
-        Serial.print("Direita \n");
         break;
       case 'a':
         analogWrite(pwmX, 190);
-        Serial.print("Esquerda \n");
         break;
       case 'w':
         analogWrite(pwmY, 190);
-        Serial.print("Frente \n");
         break;
       case 's':
         analogWrite(pwmY, 80);
-        Serial.print("Tras \n");
         break;
       default:
         analogWrite(pwmY, 135);
         analogWrite(pwmX, 135);
-        Serial.print("Parado \n");
         break;
     }
     delay(1000);
